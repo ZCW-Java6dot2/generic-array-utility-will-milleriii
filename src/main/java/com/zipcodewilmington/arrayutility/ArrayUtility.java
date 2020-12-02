a0838de10b2e325ef130e50ef6ef4df3a9bc3125
will <will@zipcodes-MBP-8.fios-router.home>
2020-12-02 08:40:39 -0500

    passed tests

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,62 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T> {
+    private T[] array;
+
+    public ArrayUtility(T[] array){
+        this.array = array;
+    }
+
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate){
+        ArrayList<T> newList = new ArrayList<>();
+        newList.addAll(Arrays.asList(array));
+        newList.addAll(Arrays.asList(arrayToMerge));
+        return Collections.frequency(newList, valueToEvaluate);
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge){
+        ArrayList<T> newList = new ArrayList<>();
+        T mostCommon = null;
+        Integer count = Integer.MIN_VALUE;
+        newList.addAll(Arrays.asList(array));
+        newList.addAll(Arrays.asList(arrayToMerge));
+        for (T t : newList){
+            Integer currentVal = Collections.frequency(newList, t);
+            if (currentVal > count){
+                mostCommon = t;
+                count = currentVal;
+            }
+        }
+        return mostCommon;
+    }
+
+    public Integer getNumberOfOccurrences(T valueToEvaluate){
+        ArrayList<T> newList = new ArrayList<>();
+        newList.addAll((Arrays.asList(array)));
+        return Collections.frequency(newList, valueToEvaluate);
+    }
+
+    public T[] removeValue(T valueToRemove){
+        int numOccurrences = getNumberOfOccurrences(valueToRemove);
+        T[] newArr = Arrays.copyOf(array, array.length - numOccurrences);
+        int i = 0;
+        int j = 0;
+        for (i = 0; i < array.length; i++){
+            if (array[i] != valueToRemove){
+                newArr[j] = array[i];
+                j++;
+            }
+        }
+        return newArr;
+    }
+
+
 }
